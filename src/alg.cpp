804fc67cd8d3d681734beea209ff677f90697233
ettennad <a.derlukova@mail.ru>
2025-04-27 01:03:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 NNTU-CS
-#include <algorithm> 
+#include <algorithm>
 
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue; 
+        if (i > 0 && arr[i] == arr[i - 1]) continue;
         for (int j = i + 1; j < len; ++j) {
             if (j > i + 1 && arr[j] == arr[j - 1]) continue;
             int sum = arr[i] + arr[j];
