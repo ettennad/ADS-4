7f30d6e96bab923def999b57cb446f8d43f83631
ettennad <a.derlukova@mail.ru>
2025-04-27 01:55:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,13 +4,10 @@
 
 int countPairs1(int* arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < len - 1; ++i) {
+    for (int i = 0; i < len; ++i) {
         for (int j = i + 1; j < len; ++j) {
-            int sum = arr[i] + arr[j];
-            if (sum == value) {
-                count++;
-            } else if (sum > value) {
-                break;
+            if (arr[i] + arr[j] == value) {
+                ++count;
             }
         }
     }
