7a592d9b918e8aff7d1cd4ae5031abf66bc5eeb6
ettennad <a.derlukova@mail.ru>
2025-04-27 01:01:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,10 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm> 
 
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; ++i) {
-        if (i > 0 && arr[i] == arr[i - 1]) continue;
+        if (i > 0 && arr[i] == arr[i - 1]) continue; 
         for (int j = i + 1; j < len; ++j) {
             if (j > i + 1 && arr[j] == arr[j - 1]) continue;
             int sum = arr[i] + arr[j];
@@ -44,7 +45,7 @@ int countPairs3(int *arr, int len, int value) {
     for (int i = 0; i < len - 1; ++i) {
         if (i > 0 && arr[i] == arr[i - 1]) continue;
         int target = value - arr[i];
-        int* it = std::lower_bound(arr + i + 1, arr + len, target);
+        auto it = std::lower_bound(arr + i + 1, arr + len, target);
         if (it != arr + len && *it == target) {
             count++;
         }
